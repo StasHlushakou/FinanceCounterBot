44ef368d246c01ba3bd8282c77ca35ca48018098
Stas Hlushakou <stas.hlushakou@gmail.com>
2021-01-09 11:49:19 +0300

    Init commit

---
+++ src/main/java/org/example/FinanceCounterBot/bot/FinanceCounterBot.java
@@ -0,0 +1,41 @@
+package org.example.FinanceCounterBot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class FinanceCounterBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.name}")
+    private String BOT_NAME;
+
+    @Value("${telegram.bot.token}")
+    private String BOT_TOKEN;
+
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
